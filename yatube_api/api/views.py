58b3eaec014644fddd110970cc251cee465cfc34
Вадим <vaden45@yandex.ru>
2021-08-26 14:32:23 +0300

    'третье_ревью'

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -47,7 +47,7 @@ class FollowViewSet(viewsets.ModelViewSet):
     search_fields = ('following__username', 'user__username')
 
     def get_queryset(self):
-        return self.request.user.follower
+        return self.request.user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
